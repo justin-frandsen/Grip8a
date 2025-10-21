d52a2f34aa0ea9246939fe03511984c279fec030
Justin Frandsen <jfran4047@gmail.com>
2025-10-21 11:41:50 -0500

    added graphing with fake data.

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', template_folder='templates')
+    return render_template('index.html')
 
+#example dynamic route
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
 
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
+
 @app.route('/stats')
-def cakes():
-    return 'Lets gooooooo!'
+def stats():
+    return "Statistics Page"
+
+@app.route('/data')
+def data():
+    # Fake time-series data
+    return jsonify({
+        'time': time.time(),
+        'force': random.uniform(20, 80)
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
